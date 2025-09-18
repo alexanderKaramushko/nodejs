b680fbe760a1b568dd1651c1087576152cbc7d88
Aleksandr Karamushko <a.morgoth.b@gmail.com>
2025-09-18 23:44:09 +0400

    redis-queue: очередь сообщений

---
+++ redis-queue/queue/src/index.ts
@@ -0,0 +1,30 @@
+import net from 'net';
+import { Redis } from 'ioredis';
+
+let redis: Redis;
+
+net
+  .createServer((socket) => {
+    redis = new Redis({
+      host: "localhost",
+      port: 6379,
+    });
+
+    redis.on('error', (error) => {
+      console.log(error.message)
+    });
+
+    socket.on('data', (imageUrl) => {
+      console.log(`Получен ${imageUrl} от ${socket.remoteAddress} на порту ${socket.remotePort}`);
+
+      redis.rpush('images', imageUrl);
+    });
+  })
+  .listen(3000)
+  .on('close', async () => {
+    await redis.flushdb();
+
+    redis.quit();
+  })
+
+console.log('Очередь сообщений запущена на порту 3000')
